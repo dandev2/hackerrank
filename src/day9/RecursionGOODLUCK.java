2cbb99ff5f038ed7163cb0c7e550dae796fba36e
dandev <dandev@naver.com>
2022-06-28 15:32:27 +0900

    hackerrank Day 9, 10 challenges 진행

--- src/day9/RecursionGOODLUCK.java
+++ src/day9/RecursionGOODLUCK.java
@@ -1,4 +1,43 @@
 package day9;
 
+import java.rmi.server.ExportException;
+
 public class RecursionGOODLUCK {
+    public static int Summation(int n) {
+        // Base Case : WE ARE AT THE END
+        if(n<=0) {
+            return 0;
+        }
+        // Recursive Case : KEEP GOING
+        else {
+            return n + Summation(n-1);
+        }
+    }
+
+
+    public static int Factorial(int n) {
+        if(n<=1) {
+            return 1;
+        }
+        else {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    public static int Exponentiation(int n, int p) {
+        // Base Case:
+        if(p<=0) {
+            return 1;
+        }
+        // Recursive
+        else {
+            return n * Exponentiation(n, p-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Summation(3));
+        System.out.println(Factorial(3));
+        System.out.println(Exponentiation(5, 3));
+    }
 }
