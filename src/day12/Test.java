cb6f651c1736486d8b17a897b100243450506bd4
dandev <dandev@naver.com>
2022-07-04 11:24:58 +0900

    Today I Learned(Hackerrank) 💡 2022/07/04 hackerrank Day 14 challenges 진행

--- src/day12/Test.java
+++ src/day12/Test.java
@@ -1,4 +1,60 @@
 package day12;
 
-public class Test {
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    Person(String firstName, String lastName, int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    public void printPerson() {
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + "\nID: " + idNumber);
+    }
+}
+
+class Student extends Person {
+    private int[] testScores;
+
+    Student(String firstName, String lastName, int id, int[] scores) {
+        super(firstName, lastName, id);
+        this.testScores = scores;
+    }
+
+    char calculate() {
+        double avg = Arrays.stream(this.testScores).average().getAsDouble();
+        if (avg > 90) return 'O';
+        if (avg >= 80) return 'E';
+        if (avg >= 70) return 'A';
+        if (avg >= 55) return 'P';
+        if (avg >= 40) return 'D';
+        return 'T';
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for (int i = 0; i < numScores; i++) {
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
+
+        Student s = new Student(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate());
+    }
 }
