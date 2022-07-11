2ba20ab1fe97fe18463b10c0df3d06103d6551e4
dandev <dandev@naver.com>
2022-07-11 15:01:46 +0900

    Today I Learned(Hackerrank) 💡 2022/07/11 hackerrank Day 19 challenges 진행

--- src/day13/Animal.java
+++ src/day13/Animal.java
@@ -1,55 +1,55 @@
-//package day13;
-//
-//
-//import org.w3c.dom.ls.LSOutput;
-//
-//public abstract class Animal {
-//    private int age; // VS private int age;
-//
-//
-//    public Animal(int age) {
-//        this.age = age;
-//        System.out.println("An animal has been created!");
-//    }
-//
-//    public abstract void eat();
-//
-//    public void sleep() {
-//        System.out.println("An animal is sleeping.");
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public static void main(String[] args) {
-//        Dog d = new Dog();
-//        Cat c = new Cat();
-//        d.eat();
-//        c.eat();
-//        d.sleep();
-//        c.sleep();
-//
-//        // Casting
-//        Object dog = new Dog();
-//        Dog realDog = (Dog) dog;
-//        realDog.ruff();
-//
-//        Object str = "est";
-//        String realS = (String) str;
-//        realS.getBytes();
-//
-//        // What happens when...
-//        Dog doggy = new Dog();
-//        if(doggy instanceof Animal) {
-//            Animal animal = (Animal) doggy;
-//            animal.sleep();
-//        }
-//        doggy.sleep();
-//    }
-//}
-//
-///*
-//ABSTRACT METHODS : DON'T HAVE A BODY
-//
-// */
+package day13;
+
+
+import org.w3c.dom.ls.LSOutput;
+
+public abstract class Animal {
+    private int age; // VS private int age;
+
+
+    public Animal(int age) {
+        this.age = age;
+        System.out.println("An animal has been created!");
+    }
+
+    public abstract void eat();
+
+    public void sleep() {
+        System.out.println("An animal is sleeping.");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Cat c = new Cat();
+        d.eat();
+        c.eat();
+        d.sleep();
+        c.sleep();
+
+        // Casting
+        Object dog = new Dog();
+        Dog realDog = (Dog) dog;
+        realDog.ruff();
+
+        Object str = "est";
+        String realS = (String) str;
+        realS.getBytes();
+
+        // What happens when...
+        Dog doggy = new Dog();
+        if(doggy instanceof Animal) {
+            Animal animal = (Animal) doggy;
+            animal.sleep();
+        }
+        doggy.sleep();
+    }
+}
+
+/*
+ABSTRACT METHODS : DON'T HAVE A BODY
+
+ */
--- src/day14/Animal.java
+++ src/day14/Animal.java
@@ -1,60 +1,60 @@
-package day14;
-
-import org.w3c.dom.ls.LSOutput;
-
-public abstract class Animal {
-    private int age; // VS private int age;
-
-
-    public Animal(int age) {
-        this.age = age;
-        System.out.println("An animal has been created!");
-    }
-
-    // f(x) = x + 5
-    // f(z, d) = z + d
-    public void doSomething(String thing) {
-        System.out.println("I'm doing " + thing);
-    }
-
-    public abstract void eat();
-
-    public void sleep() {
-        System.out.println("An animal is sleeping.");
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public static void main(String[] args) {
-        Dog d = new Dog();
-        Cat c = new Cat();
-        d.eat();
-        c.eat();
-        d.sleep();
-        c.sleep();
-
-        // Casting
-        Object dog = new Dog();
-        Dog realDog = (Dog) dog;
-        realDog.ruff();
-
-        Object str = "est";
-        String realS = (String) str;
-        realS.getBytes();
-
-        // What happens when...
-        Dog doggy = new Dog();
-        if(doggy instanceof Animal) {
-            Animal animal = (Animal) doggy;
-            animal.sleep();
-        }
-        doggy.sleep();
-    }
-}
-
-/*
-ABSTRACT METHODS : DON'T HAVE A BODY
-
- */
+//package day14;
+//
+//import org.w3c.dom.ls.LSOutput;
+//
+//public abstract class Animal {
+//    private int age; // VS private int age;
+//
+//
+//    public Animal(int age) {
+//        this.age = age;
+//        System.out.println("An animal has been created!");
+//    }
+//
+//    // f(x) = x + 5
+//    // f(z, d) = z + d
+//    public void doSomething(String thing) {
+//        System.out.println("I'm doing " + thing);
+//    }
+//
+//    public abstract void eat();
+//
+//    public void sleep() {
+//        System.out.println("An animal is sleeping.");
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public static void main(String[] args) {
+//        Dog d = new Dog();
+//        Cat c = new Cat();
+//        d.eat();
+//        c.eat();
+//        d.sleep();
+//        c.sleep();
+//
+//        // Casting
+//        Object dog = new Dog();
+//        Dog realDog = (Dog) dog;
+//        realDog.ruff();
+//
+//        Object str = "est";
+//        String realS = (String) str;
+//        realS.getBytes();
+//
+//        // What happens when...
+//        Dog doggy = new Dog();
+//        if(doggy instanceof Animal) {
+//            Animal animal = (Animal) doggy;
+//            animal.sleep();
+//        }
+//        doggy.sleep();
+//    }
+//}
+//
+///*
+//ABSTRACT METHODS : DON'T HAVE A BODY
+//
+// */
