fc9f01398513c3871361014509e478da895fb82f
dandev <dandev@naver.com>
2022-07-21 09:37:44 +0900

    💡 2022/07/21 hackerrank Day 28 challenges 진행 Today I Learned(Hackerrank)

---
+++ src/day28/User.java
@@ -0,0 +1,25 @@
+package day28;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+    String username;
+    String password;
+    int age;
+    Set<Integer> orderIDs;
+
+    public User(String customUserName, String customPassword, int customAge, Set<Integer> orderIDs) {
+        this.username = customUserName;
+        this.password = customPassword;
+        this.age = customAge;
+        this.orderIDs = orderIDs;
+    }
+
+    public static void main(String[] args) {
+        Set a = new HashSet();
+        a.add(1212);
+        User kathryn = new User("da_devel", "hello world", 10, a);
+
+    }
+}
